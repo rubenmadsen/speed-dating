979e5c83dfd587694f110619c462886d8c7a7ec2
rubenmadsen <ruben@rubenmadsen.com>
2024-01-31 16:51:08 +0100

    Added connection to mongoloid DB

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mongoose  = require("mongoose");
+const User = require("./models/user")
+require('dotenv').config();
 const app = express();
 const port = 3000;
 
@@ -6,6 +9,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+mongoose.connect(process.env.DB_SERVER).then(result => {
+  app.listen(port,function(){
+    console.log("Connected to mongo.");
+    console.log("Backend listening in port " + port + "...");
+    getAllUsers();
+  });
 });
+
+async function getAllUsers() {
+  try {
+    const users = await User.find({});
+    console.log(users);
+  } catch (err) {
+    console.error(err);
+  }
+}
