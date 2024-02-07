490da5a63e04dc311a946b914a713bade26b5dce
toha2102 <toha2102@student.miun.se>
2024-02-07 16:54:57 +0100

    Added fa-icons and grid format

--- speed-dating/src/app/footer/footer.component.ts
+++ speed-dating/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faXTwitter, faLinkedin, faFacebook, faYoutube, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  protected readonly facebook = faFacebook
+  protected readonly youtube = faYoutube
+  protected readonly linkedin = faLinkedin
+  protected readonly XTwitter = faXTwitter
+  protected readonly instagram = faInstagram
+  protected readonly github = faGithub
 
 }
