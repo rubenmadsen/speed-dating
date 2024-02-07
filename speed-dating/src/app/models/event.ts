30cb74cd030813b383c38ab3f40ebb27c5e0fbb8
rubenmadsen <ruben@rubenmadsen.com>
2024-02-07 14:19:58 +0100

    Implemented EventFeedback model interface

---
+++ speed-dating/src/app/models/event.ts
@@ -0,0 +1,18 @@
+import {Base} from "./base";
+import {User} from "./user";
+
+export interface Event extends Base{
+  startDate:Date;
+  hasEnded:boolean;
+  round:number;
+  location:string;
+  address:string;
+  city:string;
+  latitude:string;
+  longitude:string;
+  description:string;
+  totalParticipants:number;
+  currentParticipants:number;
+  participants:User[];
+  eventFeedback:EventFeedback[];
+}
