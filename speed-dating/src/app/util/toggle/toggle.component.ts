21e4d5d709b1471b80d97496201a8f9c2219fcf2
angu2106 <angu2106@student.miun.se>
2024-02-07 15:02:49 +0100

    added toggle

--- speed-dating/src/app/util/toggle/toggle.component.ts
+++ speed-dating/src/app/util/toggle/toggle.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-toggle',
   templateUrl: './toggle.component.html',
-  styleUrls: ['./toggle.component.css']
+  styleUrls: ['./toggle.component.css'],
+
 })
 export class ToggleComponent {
+  @Output() toggleShareContacts = new EventEmitter
 
+  toggleShareContact():void{
+    this.toggleShareContacts.emit()
+  }
 }
