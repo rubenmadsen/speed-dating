479a73ab27bac4bc7701dd8346aca95acc05f436
MelvinHedman <melvinhedmanco@gmail.com>
2024-02-07 14:42:05 +0100

    fixed placeholder eventpage

--- speed-dating/src/app/app-routing.module.ts
+++ speed-dating/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomepageComponent} from "./pages/homepage/homepage.component";
+import {EventPageComponent} from "./pages/event-page/event-page.component";
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
+  { path: 'event', component: EventPageComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
