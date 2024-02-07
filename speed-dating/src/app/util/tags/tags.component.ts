c7f7ec12a42ce2bb8f4ab6b79de7d40a9e35547b
angu2106 <angu2106@student.miun.se>
2024-02-07 15:47:29 +0100

    fixed small error

--- speed-dating/src/app/util/tags/tags.component.ts
+++ speed-dating/src/app/util/tags/tags.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {faClose} from "@fortawesome/free-solid-svg-icons";
+import {faClose} from "@fortawesome/free-solid-svg-icons/faClose";
 
 @Component({
   selector: 'app-tags',
