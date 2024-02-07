c211dd86e73d4f567a7acb061652eac94f4d0142
fino2101 <fino2101@student.miun.se>
2024-02-07 15:21:05 +0100

    Created match-percentage-bar

--- speed-dating/src/app/util/match-precentage-bar/match-precentage-bar.component.ts
+++ speed-dating/src/app/util/match-precentage-bar/match-precentage-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-match-precentage-bar',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./match-precentage-bar.component.css']
 })
 export class MatchPrecentageBarComponent {
+  @Input() matchValue = "50";
+  @Input() width = "5";
+  @Input() height = "1";
+  
+  ngOnInit() {
+    this.width = this.width + 'rem';
+    this.height = this.height + 'rem';
+    if (Number(this.matchValue) > 100) {
+      this.matchValue = "100";
+    }
+  }
 
+  getColor(): string {
+    if (Number(this.matchValue) < 15) {
+      return "red"
+    } else if (Number(this.matchValue) < 50) {
+      return "orange"
+    } else if (Number(this.matchValue) < 70) {
+      return "yellow"
+    } else {
+      return "green"
+    }
+  }
 }
