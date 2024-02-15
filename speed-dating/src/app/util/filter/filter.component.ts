da9cb4d69bc1a23586b0e307de4ea40e1f65d77f
toha2102 <toha2102@student.miun.se>
2024-02-15 14:13:26 +0100

    selected city is emitted

--- speed-dating/src/app/util/filter/filter.component.ts
+++ speed-dating/src/app/util/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import {BackendService} from "../../services/backend.service";
 import {EventModel} from "../../models/eventModel"
 
@@ -9,12 +9,22 @@ import {EventModel} from "../../models/eventModel"
 })
 export class FilterComponent {
   events: EventModel[]
+  selectedCity: string;
+  @Output() citySelected: EventEmitter<string> = new EventEmitter<string>();
 
   constructor(private backend: BackendService) {
     this.events = [];
+    this.selectedCity = "";
   }
 
   async ngOnInit() {
-    await this.backend.getAllEvents().then(events => this.events = events.sort((a,b) => a.startDate > b.startDate ? 1 : -1))
+    await this.backend.getAllEvents().then(events => this.events = events
+      .filter((thing, i, arr) => arr.findIndex(t => t.city === thing.city) === i)
+      .sort((a,b) => a.startDate > b.startDate ? 1 : -1))
   }
+
+  onCityChange(): void {
+    this.citySelected.emit(this.selectedCity);
+  }
+
 }
