1b40be92310d689246280da9ed15e552412d7bb6
toha2102 <toha2102@student.miun.se>
2024-02-15 10:27:40 +0100

    Added filter base with select

--- speed-dating/src/app/util/filter/filter.component.ts
+++ speed-dating/src/app/util/filter/filter.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {BackendService} from "../../services/backend.service";
+import {EventModel} from "../../models/eventModel"
 
 @Component({
   selector: 'app-filter',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent {
+  events: EventModel[]
 
+  constructor(private backend: BackendService) {
+    this.events = [];
+  }
+
+  async ngOnInit() {
+    await this.backend.getAllEvents().then(events => this.events = events.sort((a,b) => a.startDate > b.startDate ? 1 : -1))
+  }
 }
