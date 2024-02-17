015feeb0bc4e92073f67dbaabb78117a2f34a9d9
Ruben Madsen <ruben@rubenmadsen.com>
2024-02-18 00:06:36 +0100

    Fixed merge conflict problem

--- speed-dating/src/app/app.module.ts
+++ speed-dating/src/app/app.module.ts
@@ -32,13 +32,12 @@ import { CreateEventComponent } from './util/create-event/create-event.component
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BackendService } from './services/backend.service';
 import {HttpClientModule} from "@angular/common/http";
-import {DragDropModule} from '@angular/cdk/drag-drop'; 
+import {DragDropModule} from '@angular/cdk/drag-drop';
 import { FormsModule } from '@angular/forms';
 import { MinmaxSliderComponent } from './util/minmax-slider/minmax-slider.component';
 
 import { ProfileCardComponent } from './profile/profile-card/profile-card.component';
 import { ErrorbarComponent } from './util/errorbar/errorbar.component';
-import { FloorplanComponent } from './event/floorplan/floorplan.component';
 
 
 
