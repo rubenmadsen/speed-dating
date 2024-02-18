055f356598a94568abe7ca504798b29afb6c15ec
Ruben Madsen <ruben@rubenmadsen.com>
2024-02-19 00:51:14 +0100

    Added getEventsByCity on backend

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,6 +1,7 @@
 const { Router, response}    = require('express');
 const { authorizeUser } = require('../authorization/authorize')
 const Event = require("../models/eventModel")
+const City = require("../models/cityModel")
 const router = Router()
 
 /**
@@ -14,5 +15,20 @@ router.get('/event', function (req,res){
     });
 });
 
+// 65cf16aa20e795caa778d7be
+router.get('/event/city/:cityID',function (req,res){
+    City.findById(req.params.cityID).then(city => {
+        console.log("City",city);
+        Event.find({city: req.params.cityID}).then(result => {
+            console.log("params",req.params)
+            res.send(result);
+        }).catch(err => {
+            console.log(err);
+            res.status(500);
+        })
+    }).catch(err => {
+        console.log("When finding city by id", err);
+    })
 
+});
 module.exports = router;
