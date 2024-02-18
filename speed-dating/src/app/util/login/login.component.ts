a14504e7ec9188ff77bde2372467eb50fd868e6e
Ruben Madsen <ruben@rubenmadsen.com>
2024-02-18 01:09:34 +0100

    Added submit on enter and globalService message

--- speed-dating/src/app/util/login/login.component.ts
+++ speed-dating/src/app/util/login/login.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {faX} from "@fortawesome/free-solid-svg-icons/faX";
 import {BackendService} from "../../services/backend.service";
+import {StatusMessage} from "../../interfaces/statusMessage";
+import {GlobalService} from "../../services/global.service";
+import {StatusMessageType} from "../../interfaces/StatusMessageType";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -8,27 +12,47 @@ import {BackendService} from "../../services/backend.service";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
   protected readonly faX = faX;
-
   isVisible: boolean = true;
-
   errorMessage: string = '';
-
   form: any = {
     email: '',
     password: ''
   };
 
-  constructor(private backend: BackendService) {}
+  constructor(private router:Router, private backend: BackendService, private globalService:GlobalService) {}
 
+  private validateFields():boolean{
+    if (this.form.email.length === 0 || this.form.password === 0){
+      this.globalService.setGlobalStatus({message:"You need to fill out all fields", type:StatusMessageType.WARNING})
+      return false;
+    }
+    return true
+  }
   /**
    * Method to login, currently just reloads the root page with the cookie.
    */
   login() {
+    if(!this.validateFields())
+      return;
     this.backend.login(this.form.email, this.form.password).subscribe({
-      next: (response) => window.location.reload(),
-      error: (error) => {this.errorMessage = 'Login failed. Please try again.';}
+      next: (response) => {
+        this.isVisible = false;
+        const mess:StatusMessage = {
+          message:"You are logged in!",
+          type:StatusMessageType.SUCCESS
+        };
+        this.globalService.setGlobalStatus(mess);
+        setTimeout(() => this.router.navigate(['event']),500);
+        ;
+      },
+      error: (error) => {
+        this.errorMessage = 'Login failed. Please try again.';
+        const mess:StatusMessage = {} as StatusMessage;
+        mess.message = "Login failed";
+        mess.type = StatusMessageType.ALERT;
+        this.globalService.setGlobalStatus(mess);
+      }
     });
   }
   closeForm() {
