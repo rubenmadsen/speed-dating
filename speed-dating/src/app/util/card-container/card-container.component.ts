4783f96da786565933ca0e53395ac381e10bf703
MelvinHedman <melvinhedmanco@gmail.com>
2024-02-20 10:27:18 +0100

    fixed overview

--- speed-dating/src/app/util/card-container/card-container.component.ts
+++ speed-dating/src/app/util/card-container/card-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {BackendService} from "../../services/backend.service";
 import {EventModel} from "../../models/eventModel"
 
