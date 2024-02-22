3fd3af02e3125a79eb2450af3fa551f678915571
aner2112 <aner2112@student.miun.se>
2024-02-22 08:56:38 +0100

    Fixed so datecontainer creates the date

--- speed-dating/src/app/event/date-container/date-container.component.ts
+++ speed-dating/src/app/event/date-container/date-container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ParticipantListComponent} from "../participant-list/participant-list.component";
 
 @Component({
   selector: 'app-date-container',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class DateContainerComponent {
 
+
+  @Input() participantList!: ParticipantListComponent;
 }
