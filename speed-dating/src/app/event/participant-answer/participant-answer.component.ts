f5d59f7256286043face05debb808cb9a991fb12
angu2106 <angu2106@student.miun.se>
2024-02-26 15:41:19 +0100

    added a patiticipant answer to date review component

--- speed-dating/src/app/event/participant-answer/participant-answer.component.ts
+++ speed-dating/src/app/event/participant-answer/participant-answer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {DateModel} from "../../models/dateModel";
 
 @Component({
   selector: 'app-participant-answer',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./participant-answer.component.css']
 })
 export class ParticipantAnswerComponent {
+  @Input() date!: DateModel
 
+  constructor() {
+
+  }
 }
