7d2c0034869a171427f3883e278d0a8996d23e30
Ruben Madsen <ruben@rubenmadsen.com>
2024-02-26 19:15:34 +0100

    The pain is real

--- speed-dating/src/app/util/hamburger/hamburger.component.ts
+++ speed-dating/src/app/util/hamburger/hamburger.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,10 +8,15 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 })
 
 export class HamburgerComponent {
+  @ViewChild('hamRef') hamRef!:ElementRef;
+  @Output()isDiplayedChange = new EventEmitter<boolean>();
   faBars = faBars;
   isOpen:boolean = false;
 
   toggleMenu(){
     this.isOpen = !this.isOpen;
+  }
+  isDisplayed():boolean{
+    return this.hamRef.nativeElement.style.display === "none";
   }
 }
