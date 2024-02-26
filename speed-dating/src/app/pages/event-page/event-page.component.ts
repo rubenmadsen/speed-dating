5089197f39b3daa8ccce7bda0f891bd219aaf302
MelvinHedman <melvinhedmanco@gmail.com>
2024-02-26 11:31:20 +0100

    fixed buttons in eventpage

--- speed-dating/src/app/pages/event-page/event-page.component.ts
+++ speed-dating/src/app/pages/event-page/event-page.component.ts
@@ -20,6 +20,11 @@ export class EventPageComponent implements OnInit, OnDestroy {
   subscription!: Subscription;
   participants?: UserModel[];
 
+  cancelEventButtonClass: string = 'trans clr-accent border-accent';
+  clearTablesButtonClass: string = 'trans clr-accent border-accent disabled';
+  automaticMatchingButtonClass: string = 'accent border-accent clr-white disabled';
+  startDateButtonClass: string = 'accent border-accent clr-white disabled';
+
   private sub: any
 
   isOrganizer$: Observable<boolean> | undefined;
@@ -33,9 +38,25 @@ export class EventPageComponent implements OnInit, OnDestroy {
     this.subscription = this.eventService.currentEvent.subscribe(event => {
       this.event = event;
     });
+
+    const baseClass = 'trans clr-accent border-accent';
+    const disabledClass = ' disabled';
+    const accentClass = 'accent border-accent clr-white';
+
     // await this.authService.checkSession();
     this.isOrganizer$ = this.authService.isOrganizer;
     this.participants = this.event?.participants;
+
+
+    if(this.participants && this.participants.length == 20) {
+      this.clearTablesButtonClass = baseClass;
+      this.automaticMatchingButtonClass = accentClass;
+      this.startDateButtonClass = accentClass;
+    } else {
+      this.clearTablesButtonClass = baseClass + disabledClass;
+      this.automaticMatchingButtonClass = accentClass + disabledClass;
+      this.startDateButtonClass = accentClass + disabledClass;
+    }
   }
 
   ngOnDestroy() {
