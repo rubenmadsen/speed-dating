a94661e066f6466976c629e9d3aadc2dbd7abdb8
fino2101 <fino2101@student.miun.se>
2024-02-28 14:27:51 +0100

    Redirect to home if invalid subpage

--- speed-dating/src/app/app-routing.module.ts
+++ speed-dating/src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import {EventPageComponent} from "./pages/event-page/event-page.component";
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import {OverviewPageComponent} from "./pages/overview-page/overview-page.component";
 import { AuthGuardService } from './services/auth-guard.service';
+import { RatingFormComponent } from './event/rating-form/rating-form.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'event', component: EventPageComponent, canActivate: [AuthGuardService] },
   { path: 'profile', component: ProfilePageComponent, canActivate: [AuthGuardService] },
   { path: 'overview', component: OverviewPageComponent, canActivate: [AuthGuardService] },
+  {path: 'test', component: RatingFormComponent },
+  { path: '**', redirectTo: '' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
