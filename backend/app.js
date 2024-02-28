75d83eb7465a51b2350f6db9614571ca0da094da
aner2112 <aner2112@student.miun.se>
2024-02-28 13:47:14 +0100

    Moved validate/token in app.js to profile/me in userRoutes

--- backend/app.js
+++ backend/app.js
@@ -49,41 +49,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/api/validate-token", authorizeUser, async function (req, res) {
-  const user = await User.findById(req.user.id) .populate('city')
-  .populate("sharedContacts")
-  .populate({
-    path: 'events',
-    model: 'event',
-    populate: [{
-      path: 'participants',
-      model: 'user'
-    },{
-      path: 'city',
-      model: 'city'
-    }
-  ]
-    }
-  )
-  .populate({
-    path: 'activityData', // Populating activityData
-    populate: {
-      path: 'activity', // Within each activityData, populate activity
-      model: 'activity', // Ensure this matches the name you've used in mongoose.model for your Activity model
-      populate: {
-        path: 'category', // Within each activity, now populate category
-        model: 'category' // Again, ensure this matches the name used in mongoose.model for your Category model
-      }
-    }
-  });;
-  if (user) {
-    res.status(200).send({ valid: true, user: user , isOrganizer: user.isOrganizer});
-  } else {
-    res.status(404).send({ valid: false, message: "User not found" });
-  }
-});
-
-
 
 mongoose.connect(process.env.DB_SERVER).then((result) => {
   app.listen(port, function () {
